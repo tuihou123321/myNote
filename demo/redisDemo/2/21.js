3f23eddf7f468b2f1f12c0f917e477a689a25912
zhuqiao <zhuqiao@yidianling.com>
2019-11-20 15:03:40 +0800

    add

---
+++ demo/redisDemo/2/21.js
@@ -0,0 +1,10 @@
+const redis=require('redis')
+const client=require('./redis')
+
+// 1 键值对
+client.set('color', 'red', redis.print);
+client.get('color', function(err, value) {
+    if (err) throw err;
+    console.log('Got: ' + value)
+    client.quit();
+})
